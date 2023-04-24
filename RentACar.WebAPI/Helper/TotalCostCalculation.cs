30014f74bf7d45d624eb0b8f847c37b12ff5eac0
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-04-25 00:51:46 +0200

    Fix calculating of total cost

--- RentACar.WebAPI/Helper/TotalCostCalculation.cs
+++ RentACar.WebAPI/Helper/TotalCostCalculation.cs
@@ -5,6 +5,10 @@
         public static decimal Calculate(DateTime startDate, DateTime endDate, decimal dailyRate)
         {
             TimeSpan time = endDate - startDate;
+            if(time.TotalDays <= 0)
+            {
+                throw new ArgumentException("End date can't be before start date");
+            }
             //insted of int numberOfDays = time.Days
             //as for example when a person returned a car being late, they should pay extra money for the next day
             int numberOfDays = Convert.ToInt32(Math.Ceiling(time.TotalDays));
