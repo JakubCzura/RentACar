7c938f837697bb182e1577baa6778098173a962f
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-04-25 15:33:46 +0200

    Update MakeReservationDtoValidator.cs

--- RentACar.WebAPI/Validators/MakeReservationDtoValidator.cs
+++ RentACar.WebAPI/Validators/MakeReservationDtoValidator.cs
@@ -1,4 +1,4 @@
-﻿using FluentValidation;
+using FluentValidation;
 using RentACar.WebAPI.Models.Dtos;
 
 namespace RentACar.WebAPI.Validators
@@ -8,10 +8,29 @@ namespace RentACar.WebAPI.Validators
         public MakeReservationDtoValidator()
         {
             RuleFor(x => x.StartDate)
-               .NotEmpty().WithMessage("Start date can't be empty");
+               .NotEmpty().WithMessage("Start date can't be empty")
+               .NotEqual(x => x.EndDate).WithMessage("Start date can't equal end date")
+               .GreaterThanOrEqualTo(DateTime.Today);
 
             RuleFor(x => x.EndDate)
-                .NotEmpty().WithMessage("End date can't be empty");
+                .NotEmpty().WithMessage("End date can't be empty")
+                .GreaterThan(DateTime.Today);
+
+            RuleFor(x => x.PickupLocationId)
+                .NotEmpty().WithMessage("Pickup location is required")
+                .GreaterThan(0).WithMessage("Pickup location is required");
+
+            RuleFor(x => x.DropoffLocationId)
+               .NotEmpty().WithMessage("Pickup location is required")
+               .GreaterThan(0).WithMessage("Pickup location is required");
+
+            RuleFor(x => x.DropoffLocationId)
+              .NotEmpty().WithMessage("Pickup location is required")
+              .GreaterThan(0).WithMessage("Pickup location is required");
+
+            RuleFor(x => x.UserId)
+              .NotEmpty().WithMessage("User is required")
+              .GreaterThan(0).WithMessage("User is required");
         }
     }
 }
